b395e5c41c9b94703a04ba99d0a10fd375c0b912
Sam Mercer <130914459+smercer10@users.noreply.github.com>
2025-07-10 23:03:10 +0100

    Add project drawer and window selection in Doom

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -45,11 +45,11 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
+       window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
